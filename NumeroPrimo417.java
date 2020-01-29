82e31457363bde43ee8d591fd4c6c062852e4e79
JCamiloRpo <60258315+JCamiloRpo@users.noreply.github.com>
2020-01-29 08:15:51 -0500

    Update NumeroPrimo417.java

--- NumeroPrimo417.java
+++ NumeroPrimo417.java
@@ -1,18 +1,26 @@
 
 public class NumeroPrimo417 {
-	public static void main(String arg[]) {
-		int i = 1, con = 1;
-		while(con <= 417) {
-			i++;
-			int p = i-1;
-			while(p >= 1) {
-				if((i%p) == 0) break;
-				p--;
+	static class Primo{
+		public static int Posicion(int pos) {
+			int primo=1,con=1;
+			while(con <= pos) {
+				primo++;
+				if(isPrimo(primo))con++;
 			}
-			if(p <= 1)con++;
+			return primo;
 		}
-		System.out.print("Primo "+i+" en la posicion "+ (--con));
+		public static boolean isPrimo(int primo) {
+			int divisor = primo - 1;
+			while(divisor > 1) {
+				if((primo%divisor) == 0) return false;//Si se logra residuo cero quiere decir que no es primo
+				divisor--;
+			}//Sale del ciclo con el divisor = 1, por tanto si es primo
+			return true;
+		}
 	}
 	
-	
+	public static void main(String arg[]) {
+		int pos = 417;
+		System.out.print("Primo "+Primo.Posicion(pos)+" en la posicion "+ pos);
+	}
 }
