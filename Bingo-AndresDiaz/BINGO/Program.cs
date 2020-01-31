38a4b521f9b39f8085c1b261f0d147ad26311548
JCamiloRpo <60258315+JCamiloRpo@users.noreply.github.com>
2020-01-31 06:51:17 -0500

    Add files via upload
    Proyecto de bingo de Andres Diaz

---
+++ Bingo-AndresDiaz/BINGO/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BINGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Bingo = new string[] { "B", "I", "N", "G", "O" };
+            int[,] numerosAle = new int[5,5];
+            int min = 1 , max = 15;
+            var random = new Random();
+            string marcar;
+
+            for (int i = 0; i < 5; i++) {
+
+                for (int j = 0; j < 5; j++) {
+
+                    if (i == 2 && j == 2)
+                    {
+
+                        numerosAle[i, j] = 0;
+                    }
+                    else
+                    {
+
+                        numerosAle[j, i] = random.Next(min, max);
+                                           
+                    }
+                }
+                min += 15;
+                max += 15;
+            }
+            Tarjeton BINGO = new Tarjeton(Bingo, numerosAle);
+            Tarjeton BingoO = new Tarjeton(Bingo, numerosAle);
+            
+            Console.WriteLine(BINGO.Imprimir() + BingoO.Imprimir() + "\n");
+            while (1==1)
+            {
+                
+
+
+                marcar = Console.ReadLine();
+                BINGO.Marcar(marcar);
+
+                Console.WriteLine(BINGO.Imprimir()  + "\n");
+                //Console.WriteLine(BingoO.Imprimir());
+            }
+
+
+            /*for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write(Bingo[i] + " || ");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write(NumerosAle[i,j] + "||");
+
+                }
+                Console.WriteLine();
+
+            }*/
+            
+            Console.ReadKey();
+        }
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Derechos reservados (JCamilo y ADiaz)
---
+++ Bingo-AndresDiaz/BINGO/Tarjeton.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BINGO
+{
+    class Tarjeton
+    {
+
+        private string[] BINGO;
+        private int[,] NumerosAle;
+
+        public Tarjeton(string[] bINGO, int[,] numerosAle)
+        {
+            BINGO1 = bINGO;
+            NumerosAle1 = numerosAle;
+        }
+
+        public string[] BINGO1 { get => BINGO; set => BINGO = value; }
+        public int[,] NumerosAle1 { get => NumerosAle; set => NumerosAle = value; }
+
+        public string Imprimir() {
+            string imprimir = BINGO[0] + BINGO[1] + BINGO[2] + BINGO[3] + BINGO[4] + "\n";
+            /*int[,] soporte = NumerosAle;
+            string imprimir2 = imprimir;*/
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    imprimir += NumerosAle[i, j] + " || ";
+                    //imprimir2 += soporte[i, j] + " || ";
+
+                }
+                imprimir += "\n";
+                //imprimir2 += "\n";
+            }
+
+            return imprimir /*+ "                    " + imprimir2*/;
+        }
+        public int[,] Marcar(string pos) {
+
+            string[] splt = (pos.Split(','));
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (i == Convert.ToInt32(splt[0]) && j == Convert.ToInt32(splt[1]))
+                    {
+
+                        NumerosAle[i, j] = 0;
+
+                    }
+
+                }   
+            }
+            Console.Clear();
+            return NumerosAle;
+        }
+
+    }
+}
